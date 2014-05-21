416c013c1fcb080996e2e5c1c012bee3ee3b003c
gou1 <maximeraoust@gmail.com>
2014-05-21 16:20:35 +0200

    Added example

---
+++ example.php
@@ -0,0 +1,25 @@
+<?php
+
+use ManiaLib\XML\Node;
+use ManiaLib\XML\Rendering\Drivers\XMLWriterDriver;
+use ManiaLib\XML\Rendering\Renderer;
+
+require_once 'vendor/autoload.php';
+
+$root = Node::create()
+	->setNodeName('rootElement')
+	->setAttribute('rootAttrivute', '1.0');
+
+Node::create()
+	->setNodeName('someElement')
+	->setAttribute('someAttribute', 'foo')
+	->setAttribute('otherAttribute', 'bar')
+	->setNodeValue('Hello world')
+	->appendTo($root);
+
+$root->appendChild(Node::create()->setNodeName('anotherOne'));
+
+$renderer = new Renderer();
+$renderer->setDriver(new XMLWriterDriver());
+$renderer->setRoot($root);
+echo $renderer->getXML();
