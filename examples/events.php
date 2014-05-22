9f5449f12d4fadae0feacc715a2bec81852d8d3c
gou1 <maximeraoust@gmail.com>
2014-05-22 14:15:01 +0200

    Fixed example

--- examples/events.php
+++ examples/events.php
@@ -22,23 +22,23 @@ class MyCustomNode extends Node
 		// Default events names can be found in ManiaLib\XML\Rendering\Events
 		
 		// There are some "global" events
-		$dispatcher->addListener(Events::PRE_RENDER, array($this, 'preRender'));
+		$dispatcher->addListener(Events::PRE_RENDER, array($this, 'preGlobalRender'));
 		
 		// But also some events related to the current instance.
 		// ManiaLib\XML\Rendering\Events provides shortcuts to generate instance-related event names
-		$dispatcher->addListener(Events::preRender($this), array($this, 'preRender'));
+		$dispatcher->addListener(Events::preRender($this), array($this, 'preThisRender'));
 		
 		// Full documetation of Event Dispatcher at
 		// http://symfony.com/doc/current/components/event_dispatcher/index.html
 	}
 
-	function preRender()
+	function preGlobalRender()
 	{
 		// This is called before the rendering of the tree begins
 		$this->setAttribute('foo', 'bar');
 	}
 	
-	function preRender()
+	function preThisRender()
 	{
 		// This is called before the XML element related to this instance is created
 		$this->setAttribute('hello', 'world');
@@ -53,3 +53,6 @@ $renderer->setRoot($root);
 	
 header("Content-type: application/xml; charset=UTF8");
 echo $renderer->getXML();
+
+// This will output:
+// <MyCustomNode foo="bar" hello="world"/>
