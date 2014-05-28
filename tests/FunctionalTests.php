a230c356857c8b07a0c370d75962d293a3179515
satanasdiabolo <philippe@nadeo.com>
2014-05-28 16:37:38 +0200

    Created different tests for each driver Tests now scan examples folder

--- tests/FunctionalTests.php
+++ tests/FunctionalTests.php
@@ -1,9 +1,13 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Symfony\Component\Finder\Finder;
+
 class FunctionalTests extends PHPUnit_Framework_TestCase
 {
+
 	protected $examplesPath;
+
 	/**
 	 * @var \ManiaLib\XML\Rendering\Renderer
 	 */
@@ -11,15 +15,44 @@ class FunctionalTests extends PHPUnit_Framework_TestCase
 
 	protected function setUp()
 	{
-		$this->examplesPath = __DIR__.'/../examples/';
 		$this->renderer = new \ManiaLib\XML\Rendering\Renderer();
 	}
 
-	public function testBasicUsageDOMDocumentDriver()
+	public function nodeAndResultsProvider()
 	{
-		$scriptName = 'basic-usage';
-		$root = require $this->examplesPath.$scriptName.'.php';
-		$this->renderer->setRoot($root);
-		$this->assertXmlStringEqualsXmlFile($this->examplesPath.$scriptName.'.xml', $this->renderer->getXML());
+		$finder = new Finder();
+		$finder->in(__DIR__.'/../examples/')->files()->name('*.php');
+		$tests = array();
+		foreach($finder as $file)
+		{
+			$node = require $file->getRealPath();
+			$expect = $file->getPath().'/'.$file->getBasename($file->getExtension()).'xml';
+			$tests[] = array($node, $expect);
+		}
+
+		return $tests;
 	}
+
+	/**
+	 * @dataProvider nodeAndResultsProvider
+	 */
+	public function testBasicUsageDOMDocumentDriver(ManiaLib\XML\NodeInterface $node, $expectedResult)
+	{
+		$this->renderer->setRoot($node);
+		$this->assertXmlStringEqualsXmlFile($expectedResult, $this->renderer->getXML());
+	}
+
+	/**
+	 * @dataProvider nodeAndResultsProvider
+	 */
+	public function testBasicUsageXMLWriterDriver(ManiaLib\XML\NodeInterface $node, $expectedResult)
+	{
+		$driver = new \ManiaLib\XML\Rendering\Drivers\XMLWriterDriver();
+		$driver->setEventDispatcher($this->renderer->getEventDispatcher());
+		
+		$this->renderer->setRoot($node);
+		$this->renderer->setDriver($driver);
+		$this->assertXmlStringEqualsXmlFile($expectedResult, $this->renderer->getXML());
+	}
+
 }
